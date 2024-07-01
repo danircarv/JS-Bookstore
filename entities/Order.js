a9279697cc5a9a3477934172a0e3262dd46b8a5c
Daniel Rodrigues <danielrodriguescarvalho777@gmail.com>
2024-07-01 19:01:31 -0300

    Bookstore System using OOP Paradigm

---
+++ entities/Order.js
@@ -0,0 +1,22 @@
+module.exports = class Order {
+    #total
+    #items
+    #user
+    constructor(items,user) {
+        items.forEach(({product,quantity}) => {
+            if(quantity > product.inStock){
+                throw new Error(`Insufficient quantity`)
+            }
+        })
+        this.#items = items
+        this.#user = user
+        this.#total = items.reduce((total, {product,quantity}) => total + (product.price*quantity), 0)
+    }
+    get data(){
+        return{
+            items: this.#items,
+            user: this.#user,
+            total: this.#total,
+        }
+    }
+}
